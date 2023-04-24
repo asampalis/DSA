4a86f0d9111d6f5029671808d3217026ed7cea64
erichpriemer123 <90418024+erichpriemer123@users.noreply.github.com>
2023-04-24 17:02:27 -0400

    Update and rename BM.cpp to BM/main.cpp

---
+++ BM/main.cpp
@@ -0,0 +1,139 @@
+#include "string_search.h"
+
+
+bool check_files(std::string fname) {
+	std::ifstream file;
+	// opening the file
+	file.open(fname);
+
+	if (file)
+	{
+		// printing the success message
+		return true;
+	}
+	else
+	{
+		// printing the error message
+		return false;
+	}
+}
+
+
+int main() {
+
+	//Code that drives user choice between normal string search or using the autograder.
+	//normal string search just compares two strings
+	//auto grade searches multiple lines- tells you whether the question on that line is 
+	std::cout << '\n' << '\n' << '\n';
+	std::cout << "Input a for string search" << "\n";
+	std::cout << "Input b for autograder";
+	std::cout << '\n' << '\n' << '\n';
+	std::string choice;
+	std::string choice2;
+	bool chosen = true;
+	while (chosen) {
+		std::cout << "--->";
+		std::cin >> choice;
+		std::cout << "\n";
+		if (choice == "a") {
+			std::cout << "Normal string search chosen";
+			chosen = false;
+		}
+		else if (choice == "b") {
+			std::cout << "Autograder chosen";
+			chosen = false;
+		}
+		else {
+			std::cout << "Try Again!";
+		}
+		std::cout << "\n";
+	}
+	std::cout << "\n" << "\n";
+	
+	//code that drives the choice of whether the program is case sensetive or not 
+	std::cout << "Input a for case sensetive." << "\n";
+	std::cout << "Input b for non-case sensetive" << "\n";
+	bool chosen2 = true;
+	while (chosen2) {
+		std::cout << "\n" << "\n";
+		std::cout << "--->";
+		std::cin >> choice2;
+		std::cout << "\n";
+		if (choice2 == "a") {
+			std::cout << "Case sensetive chosen";
+			chosen2 = false;
+		}
+		else if (choice2 == "b") {
+			std::cout << "Non-case sensetive chosen";
+			chosen2 = false;
+		}
+		else {
+			std::cout << "Try Again!";
+		}
+		std::cout << "\n";
+	}
+	std::cout << "\n";
+
+
+	//what we should do based off the users given input 
+
+	//a is normal string search
+	if (choice == "a") {
+		
+		//get input files 
+		bool a = true;
+		std::string fname1;
+		std::string fname2;
+		while (a) {
+			std::cout << "What is the name of the key file" << "\n";
+			std::cout << "--->";
+			std::cin >> fname1;
+			std::cout << "What is the name of the search file" << "\n";
+			std::cout << "--->";
+			std::cin >> fname2;
+			
+			if (check_files(fname1) == true && check_files(fname2) == true) {
+				a = false;
+				std::cout << "Success" << "\n";
+			}
+			else {
+				std::cout << "files failed try again" << "\n";
+				std::cout << "\n" << "\n";
+			}
+		}
+
+		
+		string_key key(fname1, fname2);
+		key.printkey();
+
+		
+		int bm_time;
+		int kmp_time;
+
+		//case sensetive
+		if (choice2 == "a") 
+		{	
+			bm_time = key.BMSearch(key.answers_given[0][0], key.answers_key[0][0], key.answers_given);
+			//kmp_time = key.KMPSearch(key.answers_key[0][0], key.answers_given[0][0], -1);
+		}
+		//non case sensetive
+		else {
+			bm_time = key.caseConvert_bm(key.answers_key[0][0], key.answers_given[0][0], key.answers_given);
+			//kmp_time = key.caseConvert_kmp(key.answers_key[0][0], key.answers_given[0][0], -1);
+		}
+		
+
+		//find the times
+		std::cout << bm_time << " " ;
+		
+
+
+	}
+	else if (choice == "b") {
+		//get input files 
+
+		//if case sensitive or not 
+		//search each individual line 
+
+	}
+}
